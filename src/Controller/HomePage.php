928d02114246370965d42600113e256fb995ec30
ivan <ivan.colonneau@diji.fr>
2021-11-14 17:05:38 +0100

    home controller

--- src/Controller/HomePage.php
+++ src/Controller/HomePage.php
@@ -2,9 +2,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomePage extends AbstractControllers
+class HomePage 
 {
     /**
      * @Route("/", name="home", methods={"GET"})
